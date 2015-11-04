a1748f8d7932d29d8c40cc9db13ba90c775f1874
ASKJR <albertokatojr@gmail.com>
2015-11-04 14:00:31 -0600

    Alterações

--- ChocAn/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	public static void main(String[] args){
-		System.out.println("Hello world");
-		System.out.println("Git é legal");
-		System.out.println("Git 000");
-		// agora funcionou?
-	}
-}
