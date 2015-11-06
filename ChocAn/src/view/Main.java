3ced79cc9a2ad160f1ea6cca03fbf4384c61db7a
rsoliveira2018 <rsoliveira2018@outlook.com>
2015-11-06 16:05:00 -0600

    .
    .

--- ChocAn/src/view/Main.java
+++ ChocAn/src/view/Main.java
@@ -7,8 +7,6 @@ import dao.UserDAO;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// comment
 		
 		UserDAO uDAO = new UserDAO();
 		ArrayList<String> lstNames = uDAO.sFstNames();
@@ -16,9 +14,6 @@ public class Main {
 		for(String str : lstNames){
 			System.out.println(str);
 		}
-		
-=======
->>>>>>> ab12bbde169435757661eaad1b64dacdab7534c0
 		
 	}
 
