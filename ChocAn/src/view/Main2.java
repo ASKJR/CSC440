43a487269bf2719728407eecfc369163fe953806
ASKJR <albertokatojr@gmail.com>
2015-12-07 15:04:35 -0600

    Interface working...

--- ChocAn/src/view/Main2.java
+++ ChocAn/src/view/Main2.java
@@ -1,8 +1,23 @@
 package view;
+import beans.*;
+import dao.*;
+import controller.*;
 
 public class Main2 {
 	public static void main(String[] args){
 		Menu menu = new Menu();
+		LoginCtrl loginctrl = new LoginCtrl(); 
+		boolean notValid = true;
+		
+		while(notValid){
+			int login        =  Integer.valueOf(menu.InputLogin());
+		    String password  =  menu.InputPassword();
+		    
+		    if(loginctrl.verifyLogin(login,password)==1){
+		    	notValid = false;
+		    }
+		}
+		
 
 	}
 
--- ChocAn/src/view/Menu.java
+++ ChocAn/src/view/Menu.java
@@ -11,26 +11,48 @@ public class Menu {
 	}
 	
 	public void printMenuLogin(){
-		System.out.println("\t Login: ");
+		System.out.print("\t Login: ");
 		
 	}
 	
-	public void hello(){
-		System.out.println("Oi");
+	public void printMenuPassword(){
+		System.out.print("\t Password: ");
+		
 	}
 	
-	
-	
-	public void logicMenuLogin(){
-		String in;
+	public String InputLogin(){
+		String in = "";
 		boolean notValid = true;
 		while(notValid){
 			printMenuLogin();
 			in = sc.nextLine();
-			if(in==""){
+			if(in.equals("") || !(isNumeric(in))){
 				notValid = true;
 			}
 			else notValid = false;
 		}
+		return in;
+	}
+	
+	public String InputPassword(){
+		String in = "";
+		printMenuPassword();
+		in = sc.nextLine();
+		return in;
+	}
+	
+	
+	
+	public static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    int d = Integer.parseInt(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return false;  
+	  }  
+	  return true;  
 	}
 }
