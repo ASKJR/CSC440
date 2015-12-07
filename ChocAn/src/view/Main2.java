5d3a55510c7be00bf7272e8ecdd33f0413464a71
ASKJR <albertokatojr@gmail.com>
2015-12-07 12:23:31 -0600

    Menu do sistemem.

---
+++ ChocAn/src/view/Main2.java
@@ -0,0 +1,6 @@
+package view;
+
+public class Main2 {
+	Menu me = new Menu();
+	me.logicMenuLogin();
+}
---
+++ ChocAn/src/view/Menu.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.util.Scanner;
+
+public class Menu {
+	
+	Scanner sc ;
+	
+	public Menu(){
+		sc = new Scanner (System.in);
+	}
+	
+	public void printMenuLogin(){
+		System.out.println("\t Login: ");
+	}
+	
+	public void logicMenuLogin(){
+		String in;
+		boolean notValid = true;
+		while(notValid){
+			printMenuLogin();
+			in = sc.nextLine();
+			if(in==""){
+				notValid = true;
+			}
+			else notValid = false;
+		}
+	}
+}
