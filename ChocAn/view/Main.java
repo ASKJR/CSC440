75cf87137cd106331a7a4f67f1915489bb9fb3b9
rsoliveira2018 <rsoliveira2018@outlook.com>
2015-12-12 16:18:32 -0600

    ChocAn() reports finalizados.

--- ChocAn/view/Main.java
+++ ChocAn/view/Main.java
@@ -1,9 +1,6 @@
 package view;
 
-import java.util.Scanner;
-
 import beans.*;
-import dao.*;
 
 public class Main {
 
@@ -11,30 +8,9 @@ public class Main {
 		
 		ChocAn chocAn = new ChocAn();
 		
-		Provider provider = new Provider();
-		ProviderDAO providerDAO = new ProviderDAO();
+		String fileName = chocAn.accountsPayableSummaryFile();
 		
-		Member member = new Member();
-		MemberDAO memberDAO = new MemberDAO();
-		
-		member.setFkIdMember(2);
-		provider.setFkIdProvider(16);
-		
-		member = memberDAO.sOne(member);
-		provider = providerDAO.sOne(provider);
-		
-		Manager manager = new Manager();
-		String fileName = manager.requestWeeklyReportsFile();
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		String yesNo = "";
-		System.out.println("\n\n\t Do you want to receive this report via email (yes/no) ?");
-		yesNo = scanner.nextLine();
-		
-		if(yesNo.equals("YES") || yesNo.equals("yes")){
-			chocAn.sendReportViaEmail(fileName, "rso_oliver@hotmail.com", "Accounts Payable Report");
-		}
+		chocAn.sendReportViaEmail(fileName, "rso_oliver@hotmail.com", "Accounts Payable Summary Report");
 
 	}
 }
