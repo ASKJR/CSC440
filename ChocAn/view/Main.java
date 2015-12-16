8e73750d6723846ff4375d6a2368301ebe94db9e
rsoliveira2018 <rsoliveira2018@outlook.com>
2015-12-16 14:59:19 -0600

    Alterações na Main()

--- ChocAn/view/Main.java
+++ ChocAn/view/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Menu menu = new Menu();	
 		
+		System.out.println("\n\n\t\t\t .:: ChocAn System ::.\n");
+		menu.startSystem();
 
 	}
 }
